d5aa776c1ae22471abdad14deed1a48e8ff7b359
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2023-08-31 14:44:41 +0200

    update dialog style of ConfirmWindow

--- src/components/ConfirmWindow/ConfirmWindow.tsx
+++ src/components/ConfirmWindow/ConfirmWindow.tsx
@@ -1,9 +1,10 @@
-import { Close, Content, Overlay, Root, Title, Trigger } from '@radix-ui/react-dialog';
+import { Close, Content, Overlay, Portal, Root, Title, Trigger } from '@radix-ui/react-dialog';
 import { Cross2Icon } from '@radix-ui/react-icons';
 import * as React from 'react';
 import { FC, ReactNode } from 'react';
 import { useTranslation } from '../../hooks/useTranslation';
 import styles from './ConfirmWindow.module.scss';
+import { useH5PInstance } from '../../hooks/useH5PInstance';
 
 export type ConfirmWindowProps = {
   title: string;
@@ -25,6 +26,7 @@ export const ConfirmWindow: FC<ConfirmWindowProps> = ({
   children,
 }) => {
   const { t } = useTranslation();
+  const h5pInstance = useH5PInstance();
   const ariaLabel = t('closeDialog');
 
   const [windowOpen, setWindowOpen] = React.useState(false);
@@ -36,35 +38,39 @@ export const ConfirmWindow: FC<ConfirmWindowProps> = ({
 
   return (
     <Root open={windowOpen} onOpenChange={setWindowOpen}>
-      <Overlay className={styles.overlay} />
       <Trigger asChild>
         <button type="button" className={button.className} onClick={() => setWindowOpen(true)}>
           {button.label}
         </button>
       </Trigger>
-      <Content className={styles.confirmWindowContent}>
-        <Title className={styles.dialogTitle}>{title}</Title>
-        {children}
-        <div className={styles.confirmationButtons}>
-          <button
-            type="button"
-            className={styles.confirmButton}
-            onClick={handleConfirm}
-          >
-            {confirmWindow.confirmText}
-          </button>
-          <button
-            type="button"
-            className={styles.denyButton}
-            onClick={() => setWindowOpen(false)}
-          >
-            {confirmWindow.denyText}
-          </button>
-        </div>
-        <Close className={styles.closeButton} aria-label={ariaLabel}>
-          <Cross2Icon />
-        </Close>
-      </Content>
+      <Portal container={h5pInstance?.containerElement}>
+        <Overlay className={styles.overlay} />
+        <Content className={styles.confirmWindowContent}>
+          <div className={styles.contentWrapper}>
+            <Title className={styles.dialogTitle}>{title}</Title>
+            {children}
+            <div className={styles.confirmationButtons}>
+              <button
+                type="button"
+                className={styles.confirmButton}
+                onClick={handleConfirm}
+              >
+                {confirmWindow.confirmText}
+              </button>
+              <button
+                type="button"
+                className={styles.denyButton}
+                onClick={() => setWindowOpen(false)}
+              >
+                {confirmWindow.denyText}
+              </button>
+            </div>
+          </div>
+          <Close className={styles.closeButton} aria-label={ariaLabel}>
+            <Cross2Icon />
+          </Close>
+        </Content>
+      </Portal>
     </Root>
   );
 };
