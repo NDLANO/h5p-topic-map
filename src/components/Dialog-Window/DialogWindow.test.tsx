273dc6a67a39916a2fa6e0e566480ef59fa5dd68
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2024-06-12 17:30:15 +0200

    fix: tests for DialogWindow and DialogTabs

--- src/components/Dialog-Window/DialogWindow.test.tsx
+++ src/components/Dialog-Window/DialogWindow.test.tsx
@@ -5,13 +5,16 @@ import { TopicMapItemType } from '../../types/TopicMapItemType';
 import { Root } from '@radix-ui/react-dialog';
 
 Object.defineProperty(window, 'matchMedia', {
-  value: () => {
-    return {
-      matches: false,
-      addListener: () => { },
-      removeListener: () => { },
-    };
-  },
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
 });
 
 describe(DialogWindow.name, () => {
@@ -76,5 +79,34 @@ describe(DialogWindow.name, () => {
     setTimeout(() => {
       expect(getByText(dialogWindow, 'dialog-window-test')).toBeTruthy();
     }, 100);
+  });
+
+  it('should render the correct label', () => {
+    const item: TopicMapItemType = {
+      id: '1',
+      topicImage: {
+        path: 'https://images.unsplash.com/photo-1569587112025-0d460e81a126?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80',
+        alt: '',
+      },
+      label: 'Sheep in the distance',
+      description: 'This is a description',
+      widthPercentage: 50,
+      heightPercentage: 25,
+      xPercentagePosition: 3,
+      yPercentagePosition: 5,
+      dialog: {
+        text: 'Dialog text',
+        hasNote: true,
+        showAddLinks: false,
+      },
+    };
+
+    const dialogWindow = render(
+      <Root open={true}>
+        <DialogWindow item={item} />
+      </Root>,
+    ).container;
+
+    expect(dialogWindow.querySelector('h2')?.textContent).toBe('Sheep in the distance');
   });
 });
